76068964ea44f38eb513d283b1dc5e70cf7b5e8c
LukasTsunami <prateadow@gmail.com>
2020-05-11 14:19:13 -0300

    Adding problems that the client are passing by to understand context

---
+++ 1_problems/ninguem-quer-se-vincular-a-marcas-de-charuto.js
@@ -0,0 +1,8 @@
+export default class NinguemQuerSeVincularAMarcasDeCharuto extends Problem {
+  constructor() {
+    this.description = `
+    Depois da norma que proibe a propaganda de narcóticos no
+    Brasil, precisamos de uma nova iniciativa para que nosso
+    trabalho seja divulgado`;
+  }
+}
---
+++ 1_problems/preciso-vender-charutos.js
@@ -0,0 +1,8 @@
+export default class PrecisoVenderCharutos extends Problem {
+  constructor() {
+    this.description = `
+            Tenho uma fábrica de charutos e estou querendo
+        vender eles online.
+            São charutos cubanos para ladies and gentlemans.`;
+  }
+}
